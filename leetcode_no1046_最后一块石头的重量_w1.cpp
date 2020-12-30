8f1761dada0801b6324244ab15d811fe82eb5c6d
BestBlade <68151785+BestBlade@users.noreply.github.com>
2020-12-30 09:44:26 +0800

    Add files via upload

---
+++ leetcode_no1046_最后一块石头的重量_w1.cpp
@@ -0,0 +1,85 @@
+﻿/* ------------------------------------------------------------------|
+有一堆石头，每块石头的重量都是正整数。
+
+每一回合，从中选出两块 最重的 石头，然后将它们一起粉碎。假设石头的重量分别为 x 和 y，且 x <= y。那么粉碎的可能结果如下：
+
+如果 x == y，那么两块石头都会被完全粉碎；
+如果 x != y，那么重量为 x 的石头将会完全粉碎，而重量为 y 的石头新重量为 y-x。
+最后，最多只会剩下一块石头。返回此石头的重量。如果没有石头剩下，就返回 0。
+
+ 
+
+示例：
+
+输入：[2,7,4,1,8,1]
+输出：1
+解释：
+先选出 7 和 8，得到 1，所以数组转换为 [2,4,1,1,1]，
+再选出 2 和 4，得到 2，所以数组转换为 [2,1,1,1]，
+接着是 2 和 1，得到 1，所以数组转换为 [1,1,1]，
+最后选出 1 和 1，得到 0，最终数组转换为 [1]，这就是最后剩下那块石头的重量。
+ 
+
+提示：
+
+1 <= stones.length <= 30
+1 <= stones[i] <= 1000
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/last-stone-weight
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	优先队列法
+*
+*	执行用时：0 ms, 在所有 C++ 提交中击败了100.00%的用户
+*	内存消耗：6.2 MB, 在所有 C++ 提交中击败了72.04%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+int lastStoneWeight(vector<int>& stones) {
+    //优先队列默认大数在top
+    priority_queue<int> q;
+    for (const int& i : stones) {
+        q.emplace(i);
+    }
+    while (q.size() > 1) {
+        int u = q.top();
+        q.pop();
+        int v = q.top();
+        q.pop();
+        if (u != v) {
+            q.emplace(u - v);
+        }
+    }
+    return q.size() ? q.top() : 0;
+}
+
+int main() {
+    vector<int> stones = { 2,7,4,1,8,1 };
+    int res = lastStoneWeight(stones);
+}
